63a73a335dca538a2c5407391fe0761475b9f356
abeller3 <beller.amanda@yahoo.com>
2014-05-13 14:45:45 -0500

    added to check for scalar

--- foreach.php
+++ foreach.php
@@ -31,18 +31,24 @@
 // }
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
-foreach($things as $thing) {
-	if (is_integer($thing)) {
-		echo "{$thing} is a number\n";
-	} elseif (is_string($thing)) {
-		echo "{$thing} is a string\n";
-	} elseif (is_float($thing)) {
-		echo "{$thing} is a float\n";
-	} elseif (is_array($thing)) {
-		echo "{$thing} is an array\n";
-	} elseif (is_bool($thing)) {
-		echo "{$thing} is a boolean\n";
-	} elseif (is_null($thing)) {
-		echo "{$thing} is a null\n";
+// foreach($things as $thing) {
+// 	if (is_integer($thing)) {
+// 		echo "{$thing} is a number\n";
+// 	} elseif (is_string($thing)) {
+// 		echo "{$thing} is a string\n";
+// 	} elseif (is_float($thing)) {
+// 		echo "{$thing} is a float\n";
+// 	} elseif (is_array($thing)) {
+// 		echo "{$thing} is an array\n";
+// 	} elseif (is_bool($thing)) {
+// 		echo "{$thing} is a boolean\n";
+// 	} elseif (is_null($thing)) {
+// 		echo "{$thing} is a null\n";
+// 	}
+// 
+	foreach($things as $thing) {
+		if (is_scalar($thing)) {
+			echo "{$thing} is equivalent to a scalar.\n";
+		}
 	}
-}
+	
