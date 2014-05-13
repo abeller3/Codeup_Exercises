4f56dba4bf2455cdfd3d57cc926138d09b011943
abeller3 <beller.amanda@yahoo.com>
2014-05-13 11:56:02 -0500

    fizzbuzz

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,3 +1,21 @@
+<?php
 //for loop
 //if statments
 //modules 
+
+
+for ($i = 1; $i <= 100; $i++) {
+
+if (($i % 3 == 0) && ($i % 5 == 0)) {
+	echo "FizzBuzz\n";
+	}
+elseif ($i % 3 == 0) {
+	echo "Fizz\n";
+	}
+elseif ($i % 5 == 0) {
+	echo "Buzz\n";
+	}
+else {
+	echo "$i\n";
+}
+}
