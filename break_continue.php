61c9ba7c06cbb362142313c60a774534e7af1965
abeller3 <beller.amanda@yahoo.com>
2014-05-14 11:10:22 -0500

    for loop counting even numbers

---
+++ break_continue.php
@@ -0,0 +1,39 @@
+<?php
+
+// $numbers = array(1,2,3,4,5);
+// // Loop 1
+// foreach ($numbers as $key => $value) {
+//     echo "{$value}\n";
+//     // Loop 2
+//     for ($i = 1; $i <= 10; $i++) {
+//         if ($i == 2) {
+//             echo "{$i}\n";
+//             break 2;
+//         }
+//     }
+// }
+// echo "done!\n";
+
+
+// for ($i = 1; $i <= 100; $i+= 2) {
+//     echo $i . "\n";
+//    if ($i == 5) {
+//         continue; // exit the (for) loop
+//     } 
+// }
+// echo "DONE!\n";
+
+
+for ($i=1; $i <= 100; $i++) { 
+	if ($i % 2 == 0) {
+		echo $i . "\n";
+		continue;
+	}
+
+}
+
+for ($i=0; $i < ; $i++) { 
+	# code...
+}
+
+
