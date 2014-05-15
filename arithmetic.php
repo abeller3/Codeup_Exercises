72b9404e20229b7b29cd8a1e0932e3dd6f0f2d6b
abeller3 <beller.amanda@yahoo.com>
2014-05-15 10:00:22 -0500

    functions add/subtract etc.

---
+++ arithmetic.php
@@ -0,0 +1,37 @@
+<?php
+// function adder($a, $b)
+// {
+// 	echo ($a + $b . PHP_EOL);	
+// }
+
+// adder(38,4);
+// adder(1,2);
+
+
+
+function add($a, $b) {
+    echo $a + $b . PHP_EOL;
+}
+
+function subtract($a, $b) {
+    echo $a - $b . PHP_EOL;
+}
+
+function multiply($a, $b) {
+    echo $a * $b . PHP_EOL;
+}
+
+function divide($a, $b) {
+   echo $a / $b . PHP_EOL;
+}
+
+function modulus($a, $b) {
+	echo $a % $b . PHP_EOL;
+}
+add(3,6);
+subtract(4,2);
+multiply(5,3);
+divide(10,5);
+modulus(10,5);
+
+?>
