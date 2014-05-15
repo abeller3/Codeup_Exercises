2b741311c7bc0cca2a3a94768a583872d36fa0de
abeller3 <beller.amanda@yahoo.com>
2014-05-15 13:38:22 -0500

    corrected error statements to state variable

--- arithmetic.php
+++ arithmetic.php
@@ -1,31 +1,36 @@
 <?php
-// function add($a, $b) {
-// 	if (!(is_numeric($a)) || (!(is_numeric($b))) ) {
-// 	echo 'ERROR: both numbers entered should be numeric, brah';
-// 	} else 
-//     echo $a + $b; 
-//     echo PHP_EOL;
-// }
 
-// function subtract($a, $b) {
-// 	if (!(is_numeric($a)) || (!(is_numeric($b))) ) {
-// 	echo 'ERROR: both numbers entered should be numeric, brah';
-// 	} else 
-//     echo $a - $b;
-//     echo PHP_EOL;
-// }
+function error($a,$b)
+	if (!(is_numeric($a)) || (!(is_numeric($b))) {
+		
+	}
+function add($a, $b) {
+	if (!(is_numeric($a)) || (!(is_numeric($b))) ) {
+	echo "ERROR: either $a  or $b is not a number. Enter in numeric values only";
+	} else 
+    echo $a + $b; 
+    echo PHP_EOL;
+}
 
-// function multiply($a, $b) {
-// 	if (!(is_numeric($a)) || (!(is_numeric($b))) ) {
-// 	echo 'ERROR: both numbers entered should be numeric, brah';
-// 	} else 
-//     echo $a * $b; 
-//     echo PHP_EOL;
-// }
+function subtract($a, $b) {
+	if (!(is_numeric($a)) || (!(is_numeric($b))) ) {
+	echo "ERROR: either $a  or $b is not a number. Enter in numeric values only";
+	} else 
+    echo $a - $b;
+    echo PHP_EOL;
+}
+
+function multiply($a, $b) {
+	if (!(is_numeric($a)) || (!(is_numeric($b))) ) {
+	echo "ERROR: either $a  or $b is not a number. Enter in numeric values only";
+	} else 
+    echo $a * $b; 
+    echo PHP_EOL;
+}
 
 function divide($a, $b) {
 	if (!(is_numeric($a)) || (!(is_numeric($b))) ) {
-	echo 'ERROR: both numbers entered should be numeric, brah';
+	echo "ERROR: either $a or $b is not a number. Enter in numeric values only";
 	} elseif ($a == 0 || $b == 0) {
 	echo "Can't divide by zero"; 
 	} else 
@@ -33,16 +38,19 @@ function divide($a, $b) {
    echo PHP_EOL;
 }
 
-// function modulus($a, $b) {
-// 	if (!(is_numeric($a)) || (!(is_numeric($b))) ) {
-// 	echo 'ERROR: both numbers entered should be numeric, brah';
-// 	} else 
-// 	echo $a % $b; 
-// 	echo PHP_EOL;
-// 	}
+function modulus($a, $b) {
+	if (!(is_numeric($a)) || (!(is_numeric($b))) ) {
+	echo "ERROR: Either $a or $ b is not a number. Enter in numeric values only, brah";
+	} else 
+	echo $a % $b; 
+	echo PHP_EOL;
+	}
 
-
+add(6,9);
 divide(10,0);
+multiply(5,'love');
+modulus(10,5);
+subtract(6,1);
 
 
 
