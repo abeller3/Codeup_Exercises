6e3f4f18f1ae0f246995f16149d6e9d939df932a
abeller3 <beller.amanda@yahoo.com>
2014-05-15 13:07:16 -0500

    divide by 0 message

--- arithmetic.php
+++ arithmetic.php
@@ -1,48 +1,48 @@
 <?php
-function add($a, $b) {
-	if (!(is_numeric($a)) || (!(is_numeric($b))) ) {
-	echo 'ERROR: both numbers entered should be numeric, brah';
-	} else 
-    echo $a + $b; 
-    echo PHP_EOL;
-}
+// function add($a, $b) {
+// 	if (!(is_numeric($a)) || (!(is_numeric($b))) ) {
+// 	echo 'ERROR: both numbers entered should be numeric, brah';
+// 	} else 
+//     echo $a + $b; 
+//     echo PHP_EOL;
+// }
 
-function subtract($a, $b) {
-	if (!(is_numeric($a)) || (!(is_numeric($b))) ) {
-	echo 'ERROR: both numbers entered should be numeric, brah';
-	} else 
-    echo $a - $b;
-    echo PHP_EOL;
-}
+// function subtract($a, $b) {
+// 	if (!(is_numeric($a)) || (!(is_numeric($b))) ) {
+// 	echo 'ERROR: both numbers entered should be numeric, brah';
+// 	} else 
+//     echo $a - $b;
+//     echo PHP_EOL;
+// }
 
-function multiply($a, $b) {
-	if (!(is_numeric($a)) || (!(is_numeric($b))) ) {
-	echo 'ERROR: both numbers entered should be numeric, brah';
-	} else 
-    echo $a * $b; 
-    echo PHP_EOL;
-}
+// function multiply($a, $b) {
+// 	if (!(is_numeric($a)) || (!(is_numeric($b))) ) {
+// 	echo 'ERROR: both numbers entered should be numeric, brah';
+// 	} else 
+//     echo $a * $b; 
+//     echo PHP_EOL;
+// }
 
 function divide($a, $b) {
 	if (!(is_numeric($a)) || (!(is_numeric($b))) ) {
 	echo 'ERROR: both numbers entered should be numeric, brah';
+	} elseif ($a == 0 || $b == 0) {
+	echo "Can't divide by zero"; 
 	} else 
    echo $a / $b; 
    echo PHP_EOL;
 }
 
-function modulus($a, $b) {
-	if (!(is_numeric($a)) || (!(is_numeric($b))) ) {
-	echo 'ERROR: both numbers entered should be numeric, brah';
-	} else 
-	echo $a % $b; 
-	echo PHP_EOL;
-	}
+// function modulus($a, $b) {
+// 	if (!(is_numeric($a)) || (!(is_numeric($b))) ) {
+// 	echo 'ERROR: both numbers entered should be numeric, brah';
+// 	} else 
+// 	echo $a % $b; 
+// 	echo PHP_EOL;
+// 	}
 
-add(5,10);
-subtract('love','7');
-multiply(9,5);
-divide(10,2);
+
+divide(10,0);
 
 
 
