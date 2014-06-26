f8a1bb287de53edd2b42c1c36de84bd4e3d2a3a2
abeller3 <beller.amanda@yahoo.com>
2014-06-26 17:54:21 -0500

    fixed spacing

--- foreach.php
+++ foreach.php
@@ -1,54 +1,96 @@
 <?php 
 
-// $numbers = array(1, 2, 3, 4, 5);
-// for ($i = 0; $i < count($numbers); $i++) {
-//     echo ("\$numbers has an element with a value of {$numbers[$i]}\n");
-// }
+$numbers = array(1, 2, 3, 4, 5);
 
-// $numbers = array(1, 2, 3, 4, 5);
-// foreach ($numbers as $value) {
-//     echo ("\$number has a value of {$value}\n");
-// }
+for ($i = 0; $i < count($numbers); $i++)
+{
+    echo ("\$numbers has an element with a value of $numbers[$i]}\n");
+}
 
-// $numbers = array(1, 2, 3, 4, 5);
-// foreach ($numbers as $value) {
-//     $new_number = $value * 2;
-//     echo ("$value * 2 is {$new_number}\n");
-// }
+$numbers = array(1, 2, 3, 4, 5);
 
-// $animal_types = array('dogs', 'cats', 'birds', 'narwhals');
-// foreach ($animal_types as $animal) {
-//     echo "Old McDonald had a farm, and on that farm he raised some {$animal}\n";
-// }
+foreach ($numbers as $value) 
+{
+    echo ("\$number has a value of {$value}\n");
+}
 
-// $data = array(42, 'bacon', (6 * 3), 'The Big Bang Theory', 98.6);
-// foreach ($data as $datum) {
-//     if (is_numeric($datum)) {
-//         echo "{$datum} is a number\n";
-//     } else if (is_string($datum)) {
-//         echo "{$datum} is a string\n";
-//     }
-// }
+$numbers = array(1, 2, 3, 4, 5);
+
+foreach ($numbers as $value) 
+{
+    $new_number = $value * 2;
+    echo ("$value * 2 is {$new_number}\n");
+}
+
+$animal_types = array('dogs', 'cats', 'birds', 'narwhals');
+
+foreach ($animal_types as $animal) 
+{
+    echo "Old McDonald had a farm, and on that farm he raised some {$animal}\n";
+}
+
+$data = array(42, 'bacon', (6 * 3), 'The Big Bang Theory', 98.6);
+foreach ($data as $datum) 
+{
+    if (is_numeric($datum)) 
+    {
+        echo "{$datum} is a number\n";
+    } 
+    else if (is_string($datum))
+    {
+        echo "{$datum} is a string\n";
+    }
+}
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
-foreach($things as $thing) {
-	if (is_integer($thing)) {
-		echo "{$thing}\n";
-	} elseif (is_string($thing)) {
-		echo "{$thing}\n";
-	} elseif (is_float($thing)) {
-		echo "{$thing}\n";
-	} elseif (is_array($thing)) {
-		echo "array\n";
-	} elseif (is_bool($thing)) {
-		echo "{$thing}\n";
-	} elseif (is_null($thing)) {
-		echo "{$thing}\n";
-	} elseif (is_scalar($thing)) {
-			echo "{$thing}\n";
-		}
-	}
-	
 
-	
+foreach($things as $thing) 
+{
+    if (is_integer($thing)) 
+    {
+        echo "{$thing}\n";
+    } 
+    elseif (is_string($thing))
+    {
+        echo "{$thing}\n";
+    } 
+    elseif (is_float($thing)) 
+    {
+        echo "{$thing}\n";
+    }
+     elseif (is_array($thing)) 
+    {
+        echo "array\n";
+    } 
+    elseif (is_bool($thing)) 
+    {
+        echo "{$thing}\n";
+    } 
+    elseif (is_null($thing)) 
+    {
+        echo "{$thing}\n";
+    } 
+    elseif (is_scalar($thing))
+    {
+        echo "{$thing}\n";
+    }
+}
+    
+$a = array(
+    "one" => 1,
+    "two" => 2,
+    "three" => 3,
+    "seventeen" => 17
+);
+
+foreach ($a as $k => $v) 
+{
+    echo "\$a[$k] => $v.\n";
+}
+
+
+foreach (array(1, 2, 3, 4, 5) as $v)
+{
+    echo "$v\n";
+}   
 ?>
