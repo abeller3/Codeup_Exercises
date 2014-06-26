0598d4745e683cbf5a4fb939131e838128dd4267
abeller3 <beller.amanda@yahoo.com>
2014-06-26 16:35:42 -0500

    fixed indentions and removed php_eol where not needed

--- arithmetic.php
+++ arithmetic.php
@@ -1,60 +1,80 @@
 <?php
 
-function error($a,$b) {
-	echo "ERROR: either $a  or $b is not a number. Enter in numeric values only";
-	}
-function zero() {
-	echo "Can't divide by zero!!!";
+function error($a, $b)
+{
+  return "ERROR: Enter in numeric values only" . PHP_EOL;
 }
 
-function add($a, $b) {
-	if (!(is_numeric($a)) || (!(is_numeric($b))) ) {
-	error($a,$b);
-	} else 
-    echo $a + $b; 
-    echo PHP_EOL;
+
+function add($a, $b)
+{
+  if (!(is_numeric($a)) || (!(is_numeric($b))) )
+  {
+    echo error($a, $b);
+  } 
+  else 
+  {
+    return $a + $b;
+  }
 }
 
-function subtract($a, $b) {
-	if (!(is_numeric($a)) || (!(is_numeric($b))) ) {
-	error($a,$b);
-	} else 
-    echo $a - $b;
-    echo PHP_EOL;
+function subtract($a, $b) 
+{
+  if (!(is_numeric($a)) || (!(is_numeric($b))) ) 
+  {
+    echo error($a,$b);
+  } 
+  else 
+  {
+    return $a - $b;
+  }       
 }
 
-function multiply($a, $b) {
-	if (!(is_numeric($a)) || (!(is_numeric($b))) ) {
-	error($a,$b);
-	} else 
-    echo $a * $b; 
-    echo PHP_EOL;
+function multiply($a, $b) 
+{
+  if (!(is_numeric($a)) || (!(is_numeric($b))) )
+  {
+    echo error($a,$b);
+  } 
+  else 
+  {
+    return $a * $b; 
+  }
 }
 
 function divide($a, $b) {
-	if (!(is_numeric($a)) || (!(is_numeric($b))) ) {
-	error($a,$b);
-	} elseif ($a == 0 || $b == 0) {
-	zero();
-	} else 	
-   echo $a / $b; 
-   echo PHP_EOL;
+  if (!(is_numeric($a)) || (!(is_numeric($b))) ) 
+  {
+    echo error($a,$b);
+  } 
+  elseif ($a == 0 || $b == 0) 
+  {
+    echo "Can't divide by zero" . PHP_EOL;
+    return false;
+  } 
+  else 
+  {   
+    return $a / $b;
+  }
 }
 
 
-function modulus($a, $b) {
-	if (!(is_numeric($a)) || (!(is_numeric($b))) ) {
-	error($a,$b);
-	} else 
-	echo $a % $b; 
-	echo PHP_EOL;
-	}
+function modulus($a, $b) 
+{
+  if (!(is_numeric($a)) || (!(is_numeric($b))) ) 
+  {
+    echo error($a,$b);
+  } 
+  else
+  {
+    return $a % $b;
+  }
+}
 
-add(6,9);
-divide(10,0);
-multiply(5,'love');
-modulus(10,5);
-subtract(6,1);
-
-
+echo add('cat',2) . PHP_EOL;
+divide(3,0). PHP_EOL;
+echo subtract(6,4) . PHP_EOL;
+echo multiply(5,3) . PHP_EOL;
+echo modulus(5,4) . PHP_EOL;
+  
 
