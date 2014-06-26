b2eef9b63089326676dbedf35dfffa3450358ca1
abeller3 <beller.amanda@yahoo.com>
2014-06-26 16:46:34 -0500

    fixed indention and brackets

--- break_continue.php
+++ break_continue.php
@@ -1,40 +1,54 @@
 <?php
 
-// $numbers = array(1,2,3,4,5);
-// // Loop 1
-// foreach ($numbers as $key => $value) {
-//     echo "{$value}\n";
-//     // Loop 2
-//     for ($i = 1; $i <= 10; $i++) {
-//         if ($i == 2) {
-//             echo "{$i}\n";
-//             break 2;
-//         }
-//     }
-// }
-// echo "done!\n";
+$numbers = array(1,2,3,4,5);
 
+// Loop 1
+foreach ($numbers as $key => $value) 
+{
+	echo "{$value}\n";
+}
 
-// for ($i = 1; $i <= 100; $i+= 2) {
-//     echo $i . "\n";
-//    if ($i == 5) {
-//         continue; // exit the (for) loop
-//     } 
-// }
-// echo "DONE!\n";
+// Loop 2
+for ($i = 1; $i <= 10; $i++) 
+{
+    if ($i == 2) 
+    {
+        echo "{$i}\n";
+        break 2;
+    }
+}
 
+echo "Done!\n";
 
-// for ($i=1; $i <= 100; $i++) { 
-// 	if ($i % 2 == 0) {
-// 		echo $i . "\n";
-// 		continue;
-// 	}
+// Loop 3
+for ($i = 1; $i <= 100; $i+= 2) 
+{
+    echo $i . "\n";
+    if ($i == 5) 
+    {
+        // exit the (for) loop
+        continue; 
+    } 
+}
 
-// }
+echo "DONE!\n";
 
-for ($i=1; $i <= 100 ; $i++) {
+//Loop 4
+for ($i=1; $i <= 100; $i++) 
+{ 
+	if ($i % 2 == 0) 
+    {
+		echo $i . "\n";
+		continue;
+	}
+}
+
+//Loop 5
+for ($i=1; $i <= 100 ; $i++)
+{
 	echo "{$i}\n";
-  	if ($i >= 10) {
+  	if ($i >= 10) 
+    {
 		break;
 	}
 }
