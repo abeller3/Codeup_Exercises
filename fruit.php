5532321eeac0c1328ac93ec57fa6f0bc053dd28a
abeller3 <beller.amanda@yahoo.com>
2014-06-26 17:55:56 -0500

    fixed some spacing errors

--- fruit.php
+++ fruit.php
@@ -4,16 +4,27 @@ $fruits= ['kiwi'=> 'green','mango' => 'orange','pineapple' => 'yellow','strawber
 
 print_r($fruits); 
 
-// for ($i=0; $i < count($fruits) ; $i++) { 
-// 	$fruit=$fruits[$i];
-// 	echo $fruit .PHP_EOL;
-// }
-
-// foreach ($fruits as $key => $value) {
-// 	echo "$value is at key $key\n";
-// 	}
-
-foreach ($fruits as $key => $value) {
-	echo "$key" . "s" . " are $value\n";
+for ($i=0; $i < count($fruits) ; $i++)
+{ 
+    echo $fruits[$i] . PHP_EOL;
 }
+
+foreach ($fruits as $key => $value) 
+{
+    echo "$value is at key $key\n";
+}
+ 
+// Following Foreach's are same 
+
+
+foreach ($fruits as $key => $value)
+{
+    echo "$key" . "s" . " are $value\n";
+}
+
+foreach ($fruits as $fruit => $color)
+{
+    echo "{$fruit} are {$color}\n";
+}
+
 ?>
