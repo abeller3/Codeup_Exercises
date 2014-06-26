5a753c5c433b9a58e7f57f42cb2063063a11607a
abeller3 <beller.amanda@yahoo.com>
2014-06-26 16:52:24 -0500

    changed tab to space

--- arithmetic.php
+++ arithmetic.php
@@ -2,73 +2,74 @@
 
 function error($a, $b)
 {
-  return "ERROR: Enter in numeric values only" . PHP_EOL;
+    return "ERROR: Enter in numeric values only" . PHP_EOL;
 }
 
 
 function add($a, $b)
 {
-  if (!(is_numeric($a)) || (!(is_numeric($b))) )
-  {
-    echo error($a, $b);
-  } 
-  else 
-  {
+    if (!(is_numeric($a)) || (!(is_numeric($b))) )
+    {
+        echo error($a, $b);
+    } 
+    else 
+    {
     return $a + $b;
-  }
+    }
 }
 
 function subtract($a, $b) 
 {
-  if (!(is_numeric($a)) || (!(is_numeric($b))) ) 
-  {
-    echo error($a,$b);
-  } 
-  else 
-  {
-    return $a - $b;
-  }       
+    if (!(is_numeric($a)) || (!(is_numeric($b))) ) 
+    {
+        echo error($a,$b);
+    } 
+    else 
+    {
+        return $a - $b;
+    }       
 }
 
 function multiply($a, $b) 
 {
-  if (!(is_numeric($a)) || (!(is_numeric($b))) )
-  {
-    echo error($a,$b);
-  } 
-  else 
-  {
-    return $a * $b; 
-  }
+    if (!(is_numeric($a)) || (!(is_numeric($b))) )
+    {
+        echo error($a,$b);
+    } 
+    else 
+    {
+        return $a * $b; 
+    }
 }
 
-function divide($a, $b) {
-  if (!(is_numeric($a)) || (!(is_numeric($b))) ) 
-  {
-    echo error($a,$b);
-  } 
-  elseif ($a == 0 || $b == 0) 
-  {
-    echo "Can't divide by zero" . PHP_EOL;
-    return false;
-  } 
-  else 
-  {   
-    return $a / $b;
-  }
+function divide($a, $b) 
+{
+    if (!(is_numeric($a)) || (!(is_numeric($b))) ) 
+    {
+        echo error($a,$b);
+    } 
+    elseif ($a == 0 || $b == 0) 
+    {
+        echo "Can't divide by zero" . PHP_EOL;
+        return false;
+    } 
+    else 
+    {   
+        return $a / $b;
+    }
 }
 
 
 function modulus($a, $b) 
 {
-  if (!(is_numeric($a)) || (!(is_numeric($b))) ) 
-  {
-    echo error($a,$b);
-  } 
-  else
-  {
-    return $a % $b;
-  }
+    if (!(is_numeric($a)) || (!(is_numeric($b))) ) 
+    {
+        echo error($a,$b);
+    } 
+    else
+    {
+        return $a % $b;
+    } 
 }
 
 echo add('cat',2) . PHP_EOL;
