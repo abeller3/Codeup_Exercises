18a2085649063114fbce0a00e5e1e80586067f66
abeller3 <beller.amanda@yahoo.com>
2014-06-30 20:01:13 -0500

    put spaces between equal signs

--- do_while.php
+++ do_while.php
@@ -1,6 +1,17 @@
 <?php
-$a= 2;
+$a = 2;
+
+//loop 1
 do { 
-	echo "\$a is equal to {$a}\n";
-	$a= $a * $a;
+	echo "\$a is equal to {$a}\n" . PHP_EOL;
+	$a = $a *$a;
 } while ($a <= 1000000);
+
+//loop 2 
+$a = 100;
+
+do {
+	echo "\$a is equal to $a" . PHP_EOL;
+	$a -= 5;
+} while ($a >= 0);
+
