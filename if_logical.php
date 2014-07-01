908254f23449f7a1f5255ab183fbe6764ae83fb0
abeller3 <beller.amanda@yahoo.com>
2014-06-30 20:15:45 -0500

    corrected spacing

--- if_logical.php
+++ if_logical.php
@@ -1,27 +1,17 @@
 <?php
-// $a = 5;
-// $b = 10;
-// $c = '10';
-
-// if ($a == $b) {
-// 	echo "$a is equal to $b\n";
-// } else {
-// 	echo "$a is not equal to $b\n";
-// }
-
-// $my_age = 29;
-
-// shortcut for if or else 
-// [condition] ? [value if true] : [value if false]; 
-
-// 
 
 $a = 10;
 $b = '10';
-if ($a === $b) {
-    echo "$a is identical to $b";
-} elseif ($a == $b) {
-    echo "$a is equal to $b";
-} else {
-    echo "$a is not equal to $b";
+
+if ($a === $b) 
+{
+	echo "$a is identical to $b";
+} 
+elseif ($a == $b) 
+{
+	echo "$a is equal to $b";
+} 
+else 
+{
+	echo "$a is not equal to $b";
 }
