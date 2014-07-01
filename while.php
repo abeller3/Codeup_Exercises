8b90c02ce2b834d9323e15984529112e3ee40b24
abeller3 <beller.amanda@yahoo.com>
2014-06-30 23:28:26 -0500

    corrected spacing

--- while.php
+++ while.php
@@ -1,7 +1,15 @@
 <?php
 
 $test = 5;
-while ($test <=15) {
+while ($test <=15) 
+{
 	echo "\$test is equal to {$test}\n";
+	$test++;
+}
+
+$test = 5;
+while ($test <= 15) 
+{
+	echo $test . PHP_EOL;
 	$test++;
 }
